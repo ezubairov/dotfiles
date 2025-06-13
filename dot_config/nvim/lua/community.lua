43e5a8a006f4eb794507445f071603fa0a2e006c
Evgenii Zubairov <evgeniizubairov@gmail.com>
2025-06-13 21:35:28 +0200

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,20 +4,19 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.terraform" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.motion.mini-surround" },
-  { import = "astrocommunity.utility.noice-nvim" },
-  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
-  { import = "astrocommunity.editing-support.auto-save-nvim" },
-  { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
-  { import = "astrocommunity.recipes.auto-session-restore" },
-  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
-  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.python-ruff" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.terraform" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.motion.mini-surround" },
+	{ import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+	{ import = "astrocommunity.editing-support.auto-save-nvim" },
+	{ import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
+	{ import = "astrocommunity.recipes.auto-session-restore" },
+	{ import = "astrocommunity.bars-and-lines.vim-illuminate" },
+	{ import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+	-- import/override with your plugins folder
 }
